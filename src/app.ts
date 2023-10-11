611a302507cb783e602fe80a2f387cc184a80d3f
CrimsonLex <alexadrito10@gmail.com>
2023-10-11 12:26:04 -0500

    Correct placement of ci pipe and deleting unused routes

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import {router} from "./routes";
+import { router } from "./routes";
 import mongoose from 'mongoose';
 import db from "./config/mongo"
 import "dotenv/config";
--- src/routes/topic.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, Router } from 'express';
-
-const router = Router();
-
-router.get("/", (req:Request, res: Response) => {
-  res.send({ data: "HERE_MODELS_WILL_GO"});
-});
-
-export { router };
