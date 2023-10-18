3ee3041094b3843aef2dc70ae60caea82b1f4164
CrimsonLex <alexadrito10@gmail.com>
2023-10-18 01:48:03 -0500

    Corrections for chaining and constants

--- src/middleware/session.ts
+++ src/middleware/session.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { verifyToken } from "../utils/jwt.handle";
 import { JwtPayload } from "jsonwebtoken";
 import { RequestExt } from "../interfaces/request-extended.interface";
+import { ResponseMessage } from "../common/constants";
 
 
 const checkJwt = (req:RequestExt, res:Response, next:NextFunction) => {
@@ -10,18 +11,15 @@ const checkJwt = (req:RequestExt, res:Response, next:NextFunction) => {
         const jwt = jwtByUser.split(" ").pop() || '';
         const isUser = verifyToken(jwt);
         if(!isUser){
-            res.status(401);
-            res.send("NO_VALID_JWT_SESSION");
-
-        }else{
+            return res.status(401).send(ResponseMessage.NO_VALID_JWT_SESSION);
+        }
         req.user = isUser;
         console.log({ jwtByUser });
         next();
-        }
+        
     }catch(e) {
         console.log({ e });
-        res.status(400);
-        res.send("NO_VALID_SESSION")
+        res.status(400).send(ResponseMessage.ERROR_DURING_SESSION_CHECK)
         
     }  
 };
--- src/utils/error.handle.ts
+++ src/utils/error.handle.ts
@@ -2,8 +2,7 @@ import { Response } from "express";
 
 const handleHttp = ( res: Response, error: string, errorRaw?:any) =>{
     console.log(errorRaw);
-    res.status(500);
-    res.send({ error });
+    res.status(500).send({ error });
 
 };
 
